597ffefda294f6f851ef607302406b3a4f2690d3
Matteo Ceccarello <ceccarel@dei.unipd.it>
2014-11-12 12:19:45 +0100

    Remove ambiguity from grammar

--- clojure/parts-builder/src/parts_builder/core.clj
+++ clojure/parts-builder/src/parts_builder/core.clj
@@ -1,7 +1,14 @@
 (ns parts-builder.core
-  (:gen-class))
+  (:gen-class)
+  (:require [instaparse.core :as insta]
+            [clojure.pprint]))
+
+(def segment-parser
+  (insta/parser (clojure.java.io/resource "segments_grammar")))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [parses (insta/parses segment-parser (slurp (first args)))]
+    (println (str "There were " (count parses) " parse trees"))
+    (clojure.pprint/pprint parses)))
